05734eb116a4b21ed528252deace2508da87ee6a
Kostia Yeriomin <konstantin.yeriomin.ext@channelpilot.com>
2021-08-01 17:28:45 +0300

    Added new logic

--- KotingProgress/Program.cs
+++ KotingProgress/Program.cs
@@ -6,7 +6,7 @@ namespace KotingProgress
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
